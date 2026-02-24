c4fd0da3724135035ba0d1a43d99ffca0924d4f4
David Sanchez <dsebastiansr24@gmail.com>
2026-02-24 14:03:56 -0500

    [FIX] url

--- app/lib/strapi.ts
+++ app/lib/strapi.ts
@@ -1,5 +1,4 @@
-// export const STRAPI_BASE_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
-export const STRAPI_BASE_URL = 'http://localhost:1337';
+export const STRAPI_BASE_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 export async function getStrapiData(url: string) {
   try {
